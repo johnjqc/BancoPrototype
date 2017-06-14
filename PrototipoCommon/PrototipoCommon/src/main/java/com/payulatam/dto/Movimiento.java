20a48661a63fb3087f4a191a53b6ac7a693c35fd
John Jairo Quiroga Carvajal <john.quiroga@payulatam.com>
2017-06-14 15:46:32 -0500

    crea proyecto common con entities y dtos

---
+++ PrototipoCommon/PrototipoCommon/src/main/java/com/payulatam/dto/Movimiento.java
@@ -0,0 +1,117 @@
+package com.payulatam.dto;
+
+import com.gigaspaces.annotation.pojo.SpaceClass;
+import com.gigaspaces.annotation.pojo.SpaceId;
+import com.gigaspaces.annotation.pojo.SpaceRouting;
+
+/**
+ * A simple object used to work with the Space. Important properties include the id
+ * of the object, a type (used to perform routing when working with partitioned space),
+ * the raw data and processed data, and a boolean flag indicating if this Data object
+ * was processed or not.
+ */
+@SpaceClass
+public class Movimiento {
+
+    private String id;
+
+    private Long type;
+
+    private Boolean processed;
+    
+    private String nombre;
+    
+    private String direccion;
+    
+    private String telefono;
+
+    /**
+     * Constructs a new Data object.
+     */
+    public Movimiento() {
+
+    }
+
+    /**
+     * Constructs a new Data object with the given type
+     * and raw data.
+     */
+    public Movimiento(long type, String rawData) {
+        this.type = type;
+        this.processed = false;
+    }
+
+    /**
+     * The id of this object.
+     */
+    @SpaceId(autoGenerate=true)
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * The id of this object. Its value will be auto generated when it is written
+     * to the space.
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * The type of the data object. Used as the routing field when working with
+     * a partitioned space.
+     */
+    @SpaceRouting
+    public Long getType() {
+        return type;
+    }
+
+    /**
+     * The type of the data object. Used as the routing field when working with
+     * a partitioned space.
+     */
+    public void setType(Long type) {
+        this.type = type;
+    }
+
+    /**
+     * A boolean flag indicating if the data object was processed or not.
+     */
+    public Boolean isProcessed() {
+        return processed;
+    }
+
+    /**
+     * A boolean flag indicating if the data object was processed or not.
+     */
+    public void setProcessed(Boolean processed) {
+        this.processed = processed;
+    }
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+    
+    
+
+}
---
+++ PrototipoCommon/PrototipoCommon/src/main/java/com/payultam/entitites/Cliente.java
@@ -0,0 +1,63 @@
+package com.payultam.entitites;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the "Cliente" database table.
+ * 
+ */
+@Entity
+@Table(name="\"Cliente\"")
+@NamedQuery(name="Cliente.findAll", query="SELECT c FROM Cliente c")
+public class Cliente implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Column(name="\"Direccion\"")
+	private String direccion;
+
+	private Integer id;
+
+	@Column(name="\"Nombre\"")
+	private String nombre;
+
+	@Column(name="\"Telefono\"")
+	private String telefono;
+
+	public Cliente() {
+	}
+
+	public String getDireccion() {
+		return this.direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTelefono() {
+		return this.telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+}
