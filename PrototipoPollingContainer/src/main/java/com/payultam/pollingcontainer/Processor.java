9acb3a2aaa368c46ca468f0bf615ba6b2b3a310e
johnjqc <sir.jota@yahoo.com>
2017-06-24 17:42:44 -0500

    avance poolling container

--- PrototipoPollingContainer/src/main/java/com/payultam/pollingcontainer/Processor.java
+++ PrototipoPollingContainer/src/main/java/com/payultam/pollingcontainer/Processor.java
@@ -1,14 +1,21 @@
 package com.payultam.pollingcontainer;
 
+import java.math.BigDecimal;
 import java.util.logging.Logger;
 
 import org.openspaces.core.GigaSpace;
 import org.openspaces.events.EventDriven;
 import org.openspaces.events.EventTemplate;
+import org.openspaces.events.TransactionalEvent;
 import org.openspaces.events.adapter.SpaceDataEvent;
 import org.openspaces.events.polling.Polling;
+import org.openspaces.events.polling.ReceiveHandler;
+import org.openspaces.events.polling.receive.ReceiveOperationHandler;
+import org.openspaces.events.polling.receive.SingleTakeReceiveOperationHandler;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 
+import com.j_spaces.core.client.SQLQuery;
 import com.payulatam.model.Movement;
 
 
@@ -20,6 +27,7 @@ import com.payulatam.model.Movement;
 @EventDriven
 @Polling
 @Service
+@TransactionalEvent(isolation = Isolation.READ_COMMITTED, timeout = 1000)
 public class Processor {
 
     Logger log= Logger.getLogger(this.getClass().getName());
@@ -28,11 +36,18 @@ public class Processor {
 		System.out.println(this.getClass() + " constructed");
 	}
     
+    @ReceiveHandler
+    ReceiveOperationHandler receiveHandler() {
+        SingleTakeReceiveOperationHandler receiveHandler = new SingleTakeReceiveOperationHandler();
+        receiveHandler.setNonBlocking(true);
+        receiveHandler.setNonBlockingFactor(10);
+        return receiveHandler;
+    }
+    
     @EventTemplate
-    public Movement getTemplate() {
-    	Movement mov = new Movement();
-    	mov.setProcessed(false);
-    	return mov;
+    public SQLQuery<Movement> getTemplate() {
+    	SQLQuery<Movement> template = new SQLQuery<Movement>(Movement.class, "processed = false");
+    	return template;
     }
 
 
@@ -49,6 +64,11 @@ public class Processor {
 //        data.setData("PROCESSED : " + data.getRawData());
         System.out.println("PollingContainer : " + data.getId());
         log.info(" ------ PROCESSED : " + data);
+        
+        if (data.getValue().equals(new BigDecimal(100))) {
+        	return null;
+        }
+        
         return data;
     }
 
